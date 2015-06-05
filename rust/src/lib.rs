817c5627545bd2b9ab5c35ef82cf2a99f16650d5
Ricardo Mendes <rokusu@gmail.com>
2015-06-06 00:25:29 +0100

    Adds short_name and full_name functions

--- rust/src/lib.rs
+++ rust/src/lib.rs
@@ -1,2 +1,21 @@
 pub mod checksum;
 
+pub fn short_name(filename: Option<&str>, seconds: f64) -> Result<String, &'static str> {
+  let multiplier: f64 = 40500000f64;
+  let tick: u64 = (seconds * multiplier) as u64;
+
+  match filename {
+    Some(name) => {
+      if name.len() < 18 {
+        Ok(format!("fze1-{}.dat", name))
+      } else {
+        Err("emblem-filename should be 18 characters or less.")
+      }
+    },
+    None => Ok(format!("fze0200002000{:14X}.dat", tick as u64))
+  }
+}
+
+pub fn full_name(filename: Option<&str>) -> String {
+  format!("8P-GFZE-{}.gci", filename)
+}
