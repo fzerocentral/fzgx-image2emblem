89845daff05f41f38d1dc0054abef99985fd6f83
Ricardo Mendes <rokusu@gmail.com>
2015-06-09 04:16:21 +0100

    Extracts more blocks to separate functions

--- python/emblem.py
+++ python/emblem.py
@@ -96,6 +96,90 @@ def setup_more_info_bytes(args, now):
     return more_info_bytes
 
 
+def crop_square(img):
+    """Crop to a square.
+
+    img.size gives a tuple of (width, height).
+    Left is inclusive, right is not inclusive; same for upper and lower.
+    """
+    min_dimension = min(img.size[0], img.size[1])
+    crop_left = img.size[0] - min_dimension
+    crop_right = crop_left + min_dimension
+    crop_upper = img.size[1] - min_dimension
+    crop_lower = crop_upper + min_dimension
+    img = img.crop((crop_left, crop_upper, crop_right, crop_lower))
+
+def emblem(img64_data, alpha_threshold):
+    """Emblem (64x64)
+
+    # Go through the pixels in 4x4 blocks, left to right and top to
+    # bottom. This is the order that the emblem data must be stored in.
+    """
+    emblem_pixel_bytes = bytearray()
+
+    for block_row in xrange(16):
+        for block_col in xrange(16):
+            for pixel_row in xrange(4):
+                # Get the corresponding pixels in the 64x64 emblem, which just
+                # goes row by row.
+                first_i = block_row*64*4 + pixel_row*64 + block_col*4
+                pixel_data = [img64_data[i] for i in range(first_i, first_i+4)]
+                for rgba in pixel_data:
+                    if rgba[3] >= alpha_threshold:
+                        red = int(math.floor(rgba[0] / 8.0))
+                        green = int(math.floor(rgba[1] / 8.0))
+                        blue = int(math.floor(rgba[2] / 8.0))
+                        alpha = 1
+                        value = 32768*alpha + 1024*red + 32*green + blue
+                    else:
+                        value = 0
+                    emblem_pixel_bytes += bytearray(struct.pack(">H", value))
+    return emblem_pixel_bytes
+
+def banner(img32, alpha_threshold):
+    """Banner (96x32)
+
+    emblem_banner_base is a pre-existing file that contains the left 2/3rds
+    of an F-Zero GX emblem file's banner, in the same pixel format as any
+    emblem file. (The left 2/3rds of the banner are the same for
+    every emblem.)
+    """
+    banner_base_file = open("../common/emblem_banner_base", 'rb')
+    banner_bytes = bytearray()
+    img32_data = img32.getdata()
+
+    # We now have the banner with blank pixels in the emblem preview. Now
+    # we'll fill in that emblem preview.
+    for block_row in xrange(8):
+        banner_bytes += banner_base_file.read(0x200)
+        for block_col in xrange(8):
+            for pixel_row in xrange(4):
+                # Get the corresponding pixels in the 32x32 emblem version.
+                first_i = block_row*32*4 + pixel_row*32 + block_col*4
+                pixel_data = [img32_data[i] for i in range(first_i, first_i+4)]
+                for rgba in pixel_data:
+                    if rgba[3] >= alpha_threshold:
+                        red = int(math.floor(rgba[0] / 8.0))
+                        green = int(math.floor(rgba[1] / 8.0))
+                        blue = int(math.floor(rgba[2] / 8.0))
+                        alpha = 1
+                        value = 32768*alpha + 1024*red + 32*green + blue
+                    else:
+                        value = 0
+                    banner_bytes += bytearray(struct.pack(">H", value))
+    return banner_bytes
+
+
+def icon():
+    """Icon (32x32)
+
+    emblem_icon is a pre-existing file that contains an F-Zero GX
+    emblem file's icon, in the same pixel format as any emblem file.
+    (The icon is the same for every emblem.)
+    """
+    return open("../common/emblem_icon", 'rb').read()
+
+
 def emblem_maker(args):
     now = datetime.datetime.now()
     start_of_2000 = datetime.datetime(2000, 1, 1)
@@ -106,20 +190,10 @@ def emblem_maker(args):
 
     header_bytes = setup_header_bytes(emblem_short_filename, seconds_since_start_of_2000)
     more_info_bytes = setup_more_info_bytes(args, now)
+
     img = Image.open(args.image_filename)
-
-    # Convert the image to RGBA.
     img = img.convert(mode="RGBA")
-
-    # Crop to a square.
-    # img.size gives a tuple of (width, height).
-    # Left is inclusive, right is not inclusive; same for upper and lower.
-    min_dimension = min(img.size[0], img.size[1])
-    crop_left = img.size[0] - min_dimension
-    crop_right = crop_left + min_dimension
-    crop_upper = img.size[1] - min_dimension
-    crop_lower = crop_upper + min_dimension
-    img = img.crop((crop_left, crop_upper, crop_right, crop_lower))
+    crop_square(img)
 
     # TODO: Test non-RGBA stuff going through crop or resize64.
     # (That, or know when to tell the user to resize/convert themselves...)
@@ -152,71 +226,11 @@ def emblem_maker(args):
     # big deal though, it just means the banner may look slightly different
     # than it should in a memcard manager.
     img32 = img.resize((32,32), Image.LANCZOS)
-
-    emblem_pixel_bytes = bytearray()
     img64_data = img64.getdata()
 
-    # Emblem (64x64)
-    # Go through the pixels in 4x4 blocks, left to right and top to
-    # bottom. This is the order that the emblem data must be stored in.
-    for block_row in xrange(16):
-        for block_col in xrange(16):
-            for pixel_row in xrange(4):
-                # Get the corresponding pixels in the 64x64 emblem, which just
-                # goes row by row.
-                first_i = block_row*64*4 + pixel_row*64 + block_col*4
-                pixel_data = [img64_data[i] for i in range(first_i, first_i+4)]
-                for rgba in pixel_data:
-                    if rgba[3] >= alpha_threshold:
-                        red = int(math.floor(rgba[0] / 8.0))
-                        green = int(math.floor(rgba[1] / 8.0))
-                        blue = int(math.floor(rgba[2] / 8.0))
-                        alpha = 1
-                        value = 32768*alpha + 1024*red + 32*green + blue
-                    else:
-                        value = 0
-                    emblem_pixel_bytes += bytearray(struct.pack(">H", value))
-
-
-    # Banner (96x32)
-
-    # emblem_banner_base is a pre-existing file that contains the left 2/3rds
-    # of an F-Zero GX emblem file's banner, in the same pixel format as any
-    # emblem file. (The left 2/3rds of the banner are the same for
-    # every emblem.)
-    banner_base_file = open("../common/emblem_banner_base", 'rb')
-    banner_bytes = bytearray()
-    img32_data = img32.getdata()
-
-    # We now have the banner with blank pixels in the emblem preview. Now
-    # we'll fill in that emblem preview.
-    for block_row in xrange(8):
-        banner_bytes += banner_base_file.read(0x200)
-        for block_col in xrange(8):
-            for pixel_row in xrange(4):
-                # Get the corresponding pixels in the 32x32 emblem version.
-                first_i = block_row*32*4 + pixel_row*32 + block_col*4
-                pixel_data = [img32_data[i] for i in range(first_i, first_i+4)]
-                for rgba in pixel_data:
-                    if rgba[3] >= alpha_threshold:
-                        red = int(math.floor(rgba[0] / 8.0))
-                        green = int(math.floor(rgba[1] / 8.0))
-                        blue = int(math.floor(rgba[2] / 8.0))
-                        alpha = 1
-                        value = 32768*alpha + 1024*red + 32*green + blue
-                    else:
-                        value = 0
-                    banner_bytes += bytearray(struct.pack(">H", value))
-
-
-    # Icon (32x32)
-
-    # emblem_icon is a pre-existing file that contains an F-Zero GX
-    # emblem file's icon, in the same pixel format as any emblem file.
-    # (The icon is the same for every emblem.)
-    icon_file = open("../common/emblem_icon", 'rb')
-    icon_bytes = icon_file.read()
-
+    emblem_pixel_bytes = emblem(img64_data, alpha_threshold)
+    banner_bytes = banner(img32, alpha_threshold)
+    icon_bytes = icon()
 
     # A bunch of zeros until the end of 3 Gamecube memory blocks
     end_padding_bytes = bytearray(0x6040 - 0x40A0)
@@ -225,6 +239,8 @@ def emblem_maker(args):
       + icon_bytes + emblem_pixel_bytes + end_padding_bytes
 
     checksum_bytes = checksum(post_checksum_bytes)
+
+    print len(header_bytes) + len(checksum_bytes) + len(post_checksum_bytes)
 
     emblem_file = open(emblem_full_filename, 'wb')
     emblem_file.write(header_bytes + checksum_bytes + post_checksum_bytes)
